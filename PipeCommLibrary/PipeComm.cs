8bb631bf8881af47226f4850760be548d305fba5
Dominik Heeb <do.heeb@hotmail.com>
2015-09-18 15:55:43 +0200

    Initial Commit of first Prototype Version.

---
+++ PipeCommLibrary/PipeComm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeCommLibrary
+{
+    public static class PipeComm
+    {
+
+        public static void writePipe(string line)
+        {
+            NamedPipeClientStream clientStream = new NamedPipeClientStream("pipeDPCNet");
+            clientStream.Connect();
+            StreamWriter writer = new StreamWriter(clientStream);
+
+            writer.WriteLine(line);
+            writer.Flush();
+        }
+    }
+}
