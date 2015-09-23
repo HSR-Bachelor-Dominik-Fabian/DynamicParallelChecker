9762e9356e869f41de22a6f54d0f40766c48e058
Dominik Heeb <do.heeb@hotmail.com>
2015-09-23 13:49:59 +0200

    DPC-13 Resharper Warnings solved

--- Prototype/TestProgram/Program.cs
+++ Prototype/TestProgram/Program.cs
@@ -5,8 +5,8 @@ namespace TestProgram
 {
     class Program
     {
-        private static object a = 2;
-        private static readonly object b = 3;
+        private static object _a = 2;
+        private static readonly object _b = 3;
 
         // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
@@ -17,14 +17,14 @@ namespace TestProgram
 
         public static void Test()
         {
-            a = 123;
-            Console.WriteLine(a.ToString());
+            _a = 123;
+            Console.WriteLine(_a.ToString());
         }
 
         public static void Test2()
         {
             //TODO:Fabian: Luag das aa :)
-            Console.WriteLine(b.ToString());
+            Console.WriteLine(_b.ToString());
         }
     }
 }
